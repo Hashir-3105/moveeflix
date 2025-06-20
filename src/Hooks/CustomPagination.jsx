3db32dc0c21ea08b602e6cab37020c0a6ef3d95f
Hasnat <hasnatsarwar139@gmail.com>
2025-06-20 17:09:22 +0500

    fix case sensitive import error

--- src/Hooks/CustomPagination.jsx
+++ src/Hooks/CustomPagination.jsx
@@ -6,7 +6,7 @@ import {
     PaginationLink,
     PaginationNext,
     PaginationPrevious,
-} from "@/components/ui/pagination"
+} from "@/Components/ui/Pagination"
 
 const CustomPagination = ({currentPage , onPageChange , totalPages}) => {
     return (
